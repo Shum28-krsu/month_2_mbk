ab06f155bd655b8ec775f2988275aa17d58d4338
Baibol <musaevbaibol@gmail.com>
2026-03-26 22:11:24 +0600

    its hw 8

---
+++ homeworks/main.py
@@ -0,0 +1,7 @@
+
+from homework_8 import create_table, insert_books, get_books_by_author
+
+if __name__ == "__main__":
+    create_table()
+    insert_books()
+    get_books_by_author("George Orwell")
